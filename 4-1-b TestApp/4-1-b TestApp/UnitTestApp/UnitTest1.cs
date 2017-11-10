3804199eef68a839b6ea37056c68b3ce14c32524
AVANADE-CORP\robert.czapor <robert.czapor@avanade.com>
2017-11-10 14:46:25 +0100

    adding excersices for 3 and 4 chapter

---
+++ 4-1-b TestApp/4-1-b TestApp/UnitTestApp/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestApp.MathOperations.Double(10);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            TestApp.MathOperations.Triple(1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(20, TestApp.MathOperations.Double(10));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(30, TestApp.MathOperations.Triple(10));
+        }
+    }
+}
